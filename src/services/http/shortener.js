54d2b274d341904c61f5a3d8d693134e413729c4
IgorM-Oliveira <igor.matos.oliveira.xyz@gmail.com>
2021-12-29 21:52:07 -0400

    add update

--- src/services/http/shortener.js
+++ src/services/http/shortener.js
@@ -3,9 +3,28 @@ import swal from 'sweetalert'
 
 export default {
   url: async function (newShortener) {
-    console.log(newShortener)
     try {
       const response = await client.post('/short_register', newShortener)
+
+      if (response.data.hits > 1) {
+        try {
+          const newResponse = await client.put(`/short_update/${response.data._id}`, response.data)
+
+          document.getElementById('shortness').value = newResponse.data.shortener.code
+
+          return await swal({
+            title: 'Sucesso',
+            text: 'URL atualizada com sucesso!',
+            icon: 'success'
+          })
+        } catch (err) {
+          return await swal({
+            title: 'Oops!',
+            text: 'Atenção! Não foi possivel atualizar!',
+            icon: 'error'
+          })
+        }
+      }
 
       document.getElementById('shortness').value = response.data.shortener.code
 
